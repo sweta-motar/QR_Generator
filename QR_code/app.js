c68bed897ec01c9a7ae1f8c94b5705f6d6000ea5
sweta-motar <166835605+sweta-motar@users.noreply.github.com>
2025-09-20 19:38:06 +0530

    Delete QR_code directory

--- QR_code/app.js
+++
@@ -1,5 +0,0 @@
-var qrcode = new QRCode(document.querySelector(".qrcode"));
-qrcode.makeCode("Why did you scanned me?");
-function generateQr() {
-  qrcode.makeCode(document.querySelector("input").value);
-}

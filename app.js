072622575020cfefabd1d7774fe15348ca3711da
sweta-motar <166835605+sweta-motar@users.noreply.github.com>
2025-09-23 19:20:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
+// Create a new QRCode object
 var qrcode = new QRCode(document.querySelector(".qrcode"));
+
+// Default QR code makeCode() is a method from the QRCode.js library to create QR codes.
 qrcode.makeCode("Why did you scanned me?");
+
+// Generate QR from input
 function generateQr() {
-  qrcode.makeCode(document.querySelector("input").value);
+  var input = document.querySelector("input").value;
+  if(input.trim() === "") { // check of empty or white space
+    alert("Please enter some text or URL!");
+    return;
+  }
+
+  qrcode.makeCode(input); // generaters code
+  document.querySelector("input").value = ""; // Clear input after generating
+}
+
+// Download the QR code as an image
+function downloadQr() {
+  var qrImg = document.querySelector(".qrcode img"); // QRCode.js generates <img> inside the div
+  if(qrImg) {
+    var link = document.createElement("a");
+    link.href = qrImg.src; // set link to image
+    link.download = "QRCode.png"; // allow browser to download
+    link.click();
+  } else {
+    alert("Please generate a QR code first!");
+  }
 }
